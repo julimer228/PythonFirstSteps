0fc4c162a2e10c6041af716967eae1d7868b77d4
Julia Merta <56163818+julimer228@users.noreply.github.com>
2023-01-20 21:36:43 +0100

    Added new file

---
+++ collections.py
@@ -0,0 +1,39 @@
+# Collections and operations on collections
+# list 
+tab=[10, 20, 30, 40, 50]
+
+# negative index 
+print(tab[-1]) # print the last element
+# length of the list
+print(len(tab))
+# max element of the tab
+print(max(tab))
+# min element of the tab
+print(min(tab))
+# sum of elements
+print(sum(tab))
+# sort the list
+sorted(tab, reverse=1)
+# add element to the list 
+tab.append(45)
+# insert element to the list
+tab.insert(2, 3)
+# take an element from the last position
+x=tab.pop()
+print(x)
+# reverse list
+tab.reverse()
+
+# concate list
+list_1=[0, 1]
+list_2=[2, 3]
+list_sum=list_1+list_2
+
+# list multiplication
+list_multiplication=list_1*3
+
+# part of the list
+list=[1, 2, 3, 4, 5, 6, 7, 8]
+print(list)
+
+
